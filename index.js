23a6b624c0c2c82d74e63f49d98565439bf455c5
shahin1290 <s.patowary@yahoo.com>
2020-11-13 17:08:15 +0100

    finishes 3.13: Phonebook database, step1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -12,29 +14,6 @@ app.use(cors())
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms  :body')
 )
-
-let persons = [
-  {
-    name: 'Arto Hellas',
-    number: 040 - 123456,
-    id: 1,
-  },
-  {
-    name: 'Ada Lovelace',
-    number: 39 - 44 - 5323523,
-    id: 2,
-  },
-  {
-    name: 'Dan Abramov',
-    number: 12 - 43 - 234345,
-    id: 3,
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: 39 - 23 - 6423122,
-    id: 4,
-  },
-]
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -48,7 +27,9 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
 })
 
 const generateId = () => {
