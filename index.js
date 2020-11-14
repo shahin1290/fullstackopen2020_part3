8ea7de29dfda9e4c25e312ad1b5a3cdbad60bb5b
shahin1290 <s.patowary@yahoo.com>
2020-11-14 14:40:52 +0100

    finishes 3.19: Phonebook database, step7

--- index.js
+++ index.js
@@ -93,8 +93,12 @@ app.use(unknownEndpoint)
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
-  if (error.name === 'CastError') {
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+var uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -13,11 +14,11 @@ mongoose
     console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
+    console.log('error connecting to MongoDB:', error.message.error)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, unique: true },
   number: String,
 })
 
@@ -26,7 +27,9 @@ personSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  }
+  },
 })
+
+personSchema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('Person', personSchema)
